58ff6b814c22d8f58a87c2294dafb15eb84bc67f
ZiadHF <ziadhfahmy@yahoo.com>
2024-12-22 01:32:19 +0200

    Added function documentation and comments.

--- ivf.py
+++ ivf.py
@@ -10,19 +10,41 @@ ELEMENT_SIZE = np.dtype(np.float32).itemsize
 DIMENSION = 70
 
 class ivf:
-    def __init__(self,database_file_path = "saved_db.dat" ,clusters: int = 500, sub_cluster: int = 50, batches: int = 100000) -> None:
-        '''
-        Constructor of IVF
-        '''
+    def __init__(self, database_file_path="saved_db.dat", clusters: int = 500, sub_cluster: int = 50, batches: int = 100000) -> None:
+        """
+        Initializes an IVF (Inverted File) class instance for clustering and efficient data retrieval.
+
+        Parameters:
+        ----------
+        database_file_path : str, optional
+            The path to the file where the database is stored or will be saved. Defaults to "saved_db.dat".
+        clusters : int, optional
+            The number of clusters for the first level of clustering.
+            This determines the initial partitioningof the data space. Defaults to 500.
+        sub_cluster : int, optional
+            The number of clusters for the second level of clustering.
+            This allows more precise data retrieval. Defaults to 50.
+        batches : int, optional
+            The batch size for MiniBatchKMeans, which controls the number of samples processed at a time 
+            during clustering. Defaults to 100000.
+        """
         self.batch_size = batches
         self.cluster_num = clusters
         self.database_file_path = database_file_path
         self.sub_cluster_num = sub_cluster
 
+
     # Cosine Similarity
     def get_similarity(self, v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
         '''
         Computes the cosine similarity between two vectors or between a vector and a matrix of vectors
+
+        Parameters:
+        ----------
+        v1 : np.ndarray
+            The first vector or matrix of vectors
+        v2 : np.ndarray
+            The second vector or matrix of vectors
         '''
         v1_norm = np.linalg.norm(v1, axis=1, keepdims=True)
         v2_norm = np.linalg.norm(v2, axis=1, keepdims=True)
@@ -33,6 +55,13 @@ class ivf:
         '''
         Builds a hierarchical index of the data using multiple levels of clustering
         param data: data to build the index
+
+        Parameters:
+        ----------
+        path : str
+            The path to the directory where the index files will be stored.
+        data : np.ndarray, optional
+            The data to be indexed. If not provided, the data will be loaded from the database file.
         '''
         new_data = np.array(list(data))
         
@@ -49,15 +78,19 @@ class ivf:
         # First level clustering
         if len(new_data) >= 1000000:
             print('Using MiniBatchKMeans for first level clustering')
+            # Use MiniBatchKMeans for large datasets by choosing a random subset of the data at each iteration.
             kmeans_l1 = MiniBatchKMeans(self.cluster_num, random_state=0, 
                                         batch_size=self.batch_size, max_iter=10, n_init="auto")
+            # 
             kmeans_l1.fit(normalized_data)
             l1_centroids = kmeans_l1.cluster_centers_
             l1_labels = kmeans_l1.labels_
             print('First level clustering complete')
         else:
+            # Use kmeans2 for small datasets which takes the entire dataset at once.
             l1_centroids, l1_labels = kmeans2(normalized_data, self.cluster_num)
         
+        # Store first level centroids
         level_centroids[0] = l1_centroids
         level_centroids[1] = {}
         # Second level clustering
@@ -65,16 +98,17 @@ class ivf:
             print(f'Clustering for first level cluster {i}')
             cluster_data = normalized_data[l1_labels == i]
             
-            # Remove rows with NaN or inf values in cluster_data
+            # Remove rows with NaN or inf values in cluster_data.
             cluster_data = cluster_data[~np.isnan(cluster_data).any(axis=1)]
             cluster_data = cluster_data[~np.isinf(cluster_data).any(axis=1)]
             
-            # Debugging: Print the shape and check for NaNs or infs
+            # Printing the shape of the cluster data for debugging purposes.
             print(f'Cluster {i} data shape: {cluster_data.shape}')  
             if len(cluster_data) == 1:
                 # If the cluster contains only one vector, use it as the centroid
                 l2_centroids = cluster_data
                 l2_labels = np.array([0])
+            # If the cluster contains more than the batch size, use MiniBatchKMeans.
             elif len(cluster_data) > self.batch_size:
                 kmeans_l2 = MiniBatchKMeans(self.sub_cluster_num, random_state=0, 
                                             batch_size=self.batch_size, max_iter=10, n_init="auto")
@@ -82,28 +116,30 @@ class ivf:
                 l2_centroids = kmeans_l2.cluster_centers_
                 l2_labels = kmeans_l2.labels_
             else:
+                # Use kmeans2 for small datasets which takes the entire dataset at once.
                 l2_centroids, l2_labels = kmeans2(cluster_data, max(2, self.sub_cluster_num))
             
-            # Store second level centroids
+            # Store second level centroids.
             level_centroids[1][i] = l2_centroids
             
-            # Create leaf nodes with data indices
+            # Create leaf nodes with data indices.
             hierarchy[i] = {}
             cluster_indices = np.where(l1_labels == i)[0]
             for j in range(len(l2_centroids)):
                 sub_cluster_indices = cluster_indices[l2_labels == j]
                 hierarchy[i][j] = sub_cluster_indices.tolist()
 
-                # dump the index in the folder
+                # Dump the leaf nodes to disk.
                 with open(os.path.join(path, f'ivf_hierarchy_{i}_{j}.pkl'), 'wb') as f:
                     pickle.dump(hierarchy[i][j], f)
-
+            # Print completion message for each first level cluster.
             print(f'Clustering for first level cluster {i} complete')
         
+        # Dump the first level centroids to disk.
         with open(os.path.join(path, 'ivf_centroids.pkl'), 'wb') as f:
             pickle.dump(level_centroids, f)
         
-        # Cleanup
+        # Cleanups
         del new_data
         del normalized_data
         del hierarchy
@@ -115,7 +151,18 @@ class ivf:
     # search to find nearest index
     def find_nearest(self, path: str, query: np.ndarray, no_of_matches: int, no_of_centroids: int) -> list[int]:
         '''
-        Finds the nearest neighbors for a query vector
+        Finds the nearest neighbors for a query vector.
+
+        Parameters:
+        ----------
+        path : str
+            The path to the directory where the index files are stored.
+        query : np.ndarray
+            The query vector for which the nearest neighbors are to be found.
+        no_of_matches : int
+            The number of nearest neighbors to be found.
+        no_of_centroids : int
+            The number of centroids to be considered at the second level of the index.
         '''
         # Convert query to numpy and normalize 
         query = np.array(query).reshape(1, -1)
@@ -125,6 +172,7 @@ class ivf:
         with open(os.path.join(path, 'ivf_centroids.pkl'), 'rb') as f:
             index_centroids = pickle.load(f)
 
+        # Get first level centroids.
         first_level_centroids = index_centroids[0]
         
         # Get similarities to first level centroids
@@ -174,12 +222,23 @@ class ivf:
         candidates.sort(key=lambda x: x[2], reverse=True)
         # Return the top k nearest vectors
         return [candidate[0] for candidate in candidates[:no_of_matches]]
-
+    
     def get_one_row(self, row_num: int) -> np.ndarray:
-        # This function is only load one row in memory
+        '''
+        Loads a single row from the database file.
+
+        Parameters:
+        ----------
+        row_num : int
+            The row number of the data to be loaded.
+        '''
         try:
+            # Calculate the offset of the row in the file.
             offset = row_num * DIMENSION * ELEMENT_SIZE
+            # Use memory-mapped file to access the database.
             mmap_vector = np.memmap(self.database_file_path, dtype=np.float32, mode='r', shape=(1, DIMENSION), offset=offset)
+            # Return the row as a numpy array.
             return np.array(mmap_vector[0])
+        # Handle non-existent row numbers
         except Exception as e:
             return f"An error occurred: {e}"
--- vec_db.py
+++ vec_db.py
@@ -6,7 +6,7 @@ import os
 DB_SEED_NUMBER = 42
 ELEMENT_SIZE = np.dtype(np.float32).itemsize
 DIMENSION = 70
-INDEX_PARAMS = {
+INDEX_PARAMS = {             # Number of centroids for each database size, could be changed. Could've made it dynamic but these numbers work well.
     "no_of_centroids":{
         1: 17,
         10: 10,
@@ -17,9 +17,23 @@ INDEX_PARAMS = {
 
 class VecDB:
     def __init__(self, database_file_path = "saved_db.dat", new_db = False, db_size = None, index_file_path: str = '.') -> None:
-        
+        '''
+        Constructor for the VecDB class.
+
+        Parameters:
+        ------------
+        database_file_path: str
+            The path to the database file. Default is "saved_db.dat".
+        new_db: bool
+            If True, a new database is generated. Default is False.
+        db_size: int
+            The size of the database to be generated. Default is None.
+        index_file_path: str
+            The path to the index file. Default is '.'.
+        '''
         # INITIALIZE VARIABLES
         self.db_path = database_file_path
+        # Getting which database size is being used.
         if index_file_path == "1M_Index":
             self.size = 1
         elif index_file_path == "10M_Index":
@@ -31,65 +45,168 @@ class VecDB:
         else:
             self.size = 1
 
+        # Initialize IVF object.
         self.ivf = ivf(database_file_path)
+
+        # Set the index file path.
         self.index_file_path = index_file_path
 
-        # IF NEW DATABASE NEEDED
+        # If new_db is set to false, then the database is loaded from the file. Otherwise, a new database is generated.
         if new_db:
+            # No answer provided.
             if db_size is None:
                 raise ValueError("You need to provide the size of the database")
             # delete the old DB file if exists
             if os.path.exists(self.db_path):
                 os.remove(self.db_path)
+            # Make the directory if it doesn't exist.
             os.makedirs(self.index_file_path, exist_ok=True)
             self.generate_database(db_size)
     
     def generate_database(self, size: int) -> None:
+        '''
+        Generates a new database.
+
+        Parameters:
+        ------------
+        size: int
+            The size of the database to be generated.
+        '''
+         # Initialize a random number generator with the seed number.
         rng = np.random.default_rng(DB_SEED_NUMBER)
+        # Generate a matrix of random vectors.
         vectors = rng.random((size, DIMENSION), dtype=np.float32)
+        # Write the vectors to the database file.
         self._write_vectors_to_file(vectors)
-        print("Database generated")
+        print("Database generated.")
+        # Build the index.
         self._build_index(self.index_file_path, vectors)
 
     def _write_vectors_to_file(self, vectors: np.ndarray) -> None:
+        """
+        Writes a numpy array of vectors to a file using memory mapping for efficient storage.
+
+        Parameters:
+        ------------
+        vectors: np.ndarray
+            The array of vectors to be written to the file.
+        """
+        # Create a memory-mapped file for the vectors.
         mmap_vectors = np.memmap(self.db_path, dtype=np.float32, mode='w+', shape=vectors.shape)
+        # Copy the vectors to the memory-mapped file.
         mmap_vectors[:] = vectors[:]
+        # Flush the memory-mapped file to ensure that the data is written to disk.
         mmap_vectors.flush()
 
     def _get_num_records(self) -> int:
+        '''
+        Get the number of records in the database.
+
+        Returns:
+        ------------
+        int
+            The number of records in the database.
+        '''
         return os.path.getsize(self.db_path) // (DIMENSION * ELEMENT_SIZE)
 
     def insert_records(self, rows: Annotated[np.ndarray, (int, 70)]):
+        '''
+        Insert records into the database.
+
+        Parameters:
+        ------------
+        rows: np.ndarray
+            The array of vectors to be inserted into the database.
+        '''
+        # Get the number of records in the database.
         num_old_records = self._get_num_records()
+        # Calculate the new shape of the memory-mapped file.
         num_new_records = len(rows)
         full_shape = (num_old_records + num_new_records, DIMENSION)
+        # Open the memory-mapped file in read-write mode.
         mmap_vectors = np.memmap(self.db_path, dtype=np.float32, mode='r+', shape=full_shape)
         mmap_vectors[num_old_records:] = rows
+        # Flush the memory-mapped file to ensure that the data is written to disk.
         mmap_vectors.flush()
         #TODO: might change to call insert in the index, if you need
         self._build_index(self.index_file_path, rows)
 
     def get_one_row(self, row_num: int) -> np.ndarray:
-        # This function is only load one row in memory
+        '''
+        Get one row from the database.
+
+        Parameters:
+        ------------
+        row_num: int
+            The row number to be retrieved.
+        
+        Returns:
+        ------------
+        np.ndarray
+            The vector at the specified row number.
+        '''
         try:
+            # Calculate the offset of the row in the memory-mapped file.
             offset = row_num * DIMENSION * ELEMENT_SIZE
             mmap_vector = np.memmap(self.db_path, dtype=np.float32, mode='r', shape=(1, DIMENSION), offset=offset)
             return np.array(mmap_vector[0])
+        # Handle any exceptions that occur during the process.
         except Exception as e:
             return f"An error occurred: {e}"
 
     def get_all_rows(self) -> np.ndarray:
+        '''
+        Get all rows from the database.
+
+        Returns:
+        ------------
+        np.ndarray
+            The array of vectors in the database.
+        '''
         # Take care this load all the data in memory
         num_records = self._get_num_records()
+        # Use a memory-mapped file to access the database file.
         vectors = np.memmap(self.db_path, dtype=np.float32, mode='r', shape=(num_records, DIMENSION))
         return np.array(vectors)
     
     def retrieve(self, query: Annotated[np.ndarray, (1, DIMENSION)], top_k = 5):
+        """
+        Retrieves the top-k most similar vectors to the query from the index.
+
+        Parameters:
+        ------------
+        query : Annotated[np.ndarray, (1, DIMENSION)]
+            The query vector of shape (1, DIMENSION) used for similarity search.
+        top_k : int, optional
+            The number of closest vectors to retrieve. Defaults to 5.
+
+        Returns:
+        ------------
+        list
+            A list of the top-k most similar vectors to the query, ranked by similarity.
+        """
+        # Get the number of centroids to use for the search.
         no_of_centroids = INDEX_PARAMS["no_of_centroids"][self.size]
+        # Perform the similarity search using the IVF index.
         results = self.ivf.find_nearest(self.index_file_path, query, top_k, no_of_centroids)
         return results
     
     def _cal_score(self, vec1, vec2):
+        """
+        Calculate the cosine similarity between two vectors.
+
+        Parameters:
+        ------------
+        vec1 : np.ndarray
+            The first vector.
+        vec2 : np.ndarray
+            The second vector.
+        
+        Returns:
+        ------------
+        float
+            The cosine similarity between the two vectors.
+        """
         dot_product = np.dot(vec1, vec2)
         norm_vec1 = np.linalg.norm(vec1)
         norm_vec2 = np.linalg.norm(vec2)
@@ -97,4 +214,14 @@ class VecDB:
         return cosine_similarity
 
     def _build_index(self, path, data = None):
+        """
+        Build the index for the database.
+
+        Parameters:
+        ------------
+        path : str
+            The path to the index file.
+        data : np.ndarray, optional
+            The data to be indexed. Defaults to None.
+        """
         self.ivf.build_index(path, data)
